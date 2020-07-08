1df48b315a9c9d444826584018e09c6d3ac0c022
George Karfakis <gk819@ic.ac.uk>
2020-07-09 01:02:13 +0300

    Downloading from cloudinary

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,21 @@
 from flask import Flask
 from config import Config
 from flask_bootstrap import Bootstrap
-import cloudinary
+from PIL import Image
+import requests
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 bootstrap=Bootstrap(app)
 
-im=cloudinary.CloudinaryImage("sample_rtt6io.jpg").image(quality=100)
-f=open("app/static/sample2.png","w+")
-f.write(im)
-f.close()
+file=requests.get("http://res.cloudinary.com/gkwebsite/image/upload/sample_rtt6io.png")
+with open("app/static/sample.png", "wb+") as op:
+    op.write(file.content)
+
+file=requests.get("http://res.cloudinary.com/gkwebsite/image/upload/sample.png")
+with open("app/static/sample2.png", "wb+") as op:
+    op.write(file.content)
 
 from app import routes
 
